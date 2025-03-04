63d3d057bb1ace34c18282816f80818917beeadb
sarbashreeeeee <sapkota.sarbashree222@gmail.com>
2025-03-04 22:18:29 +0545

    UI idea for metrics input page

---
+++ app/blueprints/disease_detection/forms/metrics_input.py
@@ -0,0 +1,115 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, FloatField, RadioField, StringField
+from wtforms.validators import DataRequired, NumberRange, Optional
+
+
+class HealthMetricsForm(FlaskForm):
+    # Personal Information
+    age = IntegerField(
+        "Age",
+        validators=[
+            DataRequired(),
+            NumberRange(min=18, max=120, message="Age must be between 18 and 120"),
+        ],
+    )
+    gender = RadioField(
+        "Gender",
+        choices=[("male", "Male"), ("female", "Female")],
+        validators=[DataRequired()],
+    )
+
+    # Lifestyle Factors
+    smoker = RadioField(
+        "Current Smoker?",
+        choices=[("yes", "Yes"), ("no", "No")],
+        validators=[DataRequired()],
+    )
+    cigarettes = IntegerField(
+        "Cigarettes per day",
+        validators=[
+            Optional(),
+            NumberRange(
+                min=1,
+                max=100,
+                message="Please enter a valid number of cigarettes (1-100)",
+            ),
+        ],
+    )
+
+    # Medical History
+    bp_meds = RadioField(
+        "Taking BP Medicines?",
+        choices=[("yes", "Yes"), ("no", "No")],
+        validators=[DataRequired()],
+    )
+    stroke = RadioField(
+        "Previous Stroke History?",
+        choices=[("yes", "Yes"), ("no", "No")],
+        validators=[DataRequired()],
+    )
+    hypertension = RadioField(
+        "Hypertension?",
+        choices=[("yes", "Yes"), ("no", "No")],
+        validators=[DataRequired()],
+    )
+    diabetes = RadioField(
+        "Diabetes?", choices=[("yes", "Yes"), ("no", "No")], validators=[DataRequired()]
+    )
+
+    # Current Health Metrics
+    systolic = IntegerField(
+        "Systolic Blood Pressure",
+        validators=[
+            DataRequired(),
+            NumberRange(
+                min=70, max=250, message="Systolic BP must be between 70 and 250 mmHg"
+            ),
+        ],
+    )
+    diastolic = IntegerField(
+        "Diastolic Blood Pressure",
+        validators=[
+            DataRequired(),
+            NumberRange(
+                min=40, max=150, message="Diastolic BP must be between 40 and 150 mmHg"
+            ),
+        ],
+    )
+    cholesterol = IntegerField(
+        "Total Cholesterol",
+        validators=[
+            DataRequired(),
+            NumberRange(
+                min=100,
+                max=600,
+                message="Cholesterol must be between 100 and 600 mg/dL",
+            ),
+        ],
+    )
+    glucose = IntegerField(
+        "Blood Glucose",
+        validators=[
+            DataRequired(),
+            NumberRange(
+                min=40,
+                max=400,
+                message="Blood glucose must be between 40 and 400 mg/dL",
+            ),
+        ],
+    )
+    heart_rate = IntegerField(
+        "Heart Rate",
+        validators=[
+            DataRequired(),
+            NumberRange(
+                min=40, max=200, message="Heart rate must be between 40 and 200 bpm"
+            ),
+        ],
+    )
+    bmi = FloatField(
+        "BMI",
+        validators=[
+            DataRequired(),
+            NumberRange(min=10, max=50, message="BMI must be between 10 and 50"),
+        ],
+    )
