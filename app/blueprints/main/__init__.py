a0feb370f2f88a4c6cecbc3f311e20999c7ce285
sarbashreeeeee <sapkota.sarbashree222@gmail.com>
2025-04-03 20:55:20 +0545

    Minor changes in the landing page

--- app/blueprints/main/__init__.py
+++ app/blueprints/main/__init__.py
@@ -1,3 +1,9 @@
 from flask import Blueprint
 
-main_bp = Blueprint("main", __name__, template_folder="templates")
+main_bp = Blueprint(
+    "main",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+    static_url_path="/app/blueprints/main/static",
+)
